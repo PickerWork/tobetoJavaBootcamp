716af78d059a2fee9ddcb36022b42dc4d7d82d80
PickerWork <pickerwork@gmail.com>
2023-12-07 20:10:42 +0300

    Stream Api

--- springProject/src/main/java/com/tobeto/springProject/PersonsController.java
+++ springProject/src/main/java/com/tobeto/springProject/PersonsController.java
@@ -1,23 +1,40 @@
 package com.tobeto.springProject;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/persons") // Controllerın kontrol edeceği alt routerları tanımlar
 public class PersonsController {
+    List<Person> inMemoryList = new ArrayList<>();
     @GetMapping
-    public String get(){ //api/persons
-        return "Get 1";
+    public List<Person> get(){ //api/persons
+        return inMemoryList;
     }
-    @GetMapping("get2") //api/persons/get2
-    public String get2(){
-        return "Get 2";
-    }
+    //@GetMapping("getById") //api/persons/get2 süslü parantez dinamik olarak değişeceğini ifade eder
+    //query string, path variable = kullanıcan veri alma işlemleri
+    //path https://localhost:8080/api/persons/1 => 1 = id
+    //query string yine url den gönderiyoruz fakat "?" işareti ekliyoruz https://localhost:8080/api/persons?id=1
+    @GetMapping("{id}")
+    public Person getById(@PathVariable int id){ //query de @RequestParam
+        //Lambda Expressions
+        //Stream API
+        Person person = inMemoryList.stream().filter((person1)->person1.getId() == id).findFirst().orElseThrow();
+        return person;
+    } //@Pathvariable eklemen lazım id nin pathden geleceğini söyler.
     @PostMapping
-    public String post(){
-        return "Tobeto";
+    public void add(@RequestBody Person person){
+            inMemoryList.add(person);
+    }
+    @PutMapping
+    public void update(@RequestBody Person person){
+
+    }
+    @DeleteMapping ("{id}")
+    public void delete(@PathVariable int id){
+
     }
 }
