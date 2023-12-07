3335f4ffd5cf93922dd37d23a84cb067651beda7
PickerWork <pickerwork@gmail.com>
2023-12-07 15:05:16 +0300

    Unique Characters

---
+++ pairWorkshopFirst/uniqueCharacters/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir kelime girin: ");
+        String metin = scanner.nextLine();
+        scanner.close();
+
+        if (hasAllUniqueCharacters(metin)) {
+            System.out.println("Kelime tüm benzersiz karakterlere sahiptir.");
+        } else {
+            System.out.println("Kelime tüm benzersiz karakterlere sahip değildir.");
+        }
+    }
+
+    static boolean hasAllUniqueCharacters(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            if (str.indexOf(currentChar) != str.lastIndexOf(currentChar)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
