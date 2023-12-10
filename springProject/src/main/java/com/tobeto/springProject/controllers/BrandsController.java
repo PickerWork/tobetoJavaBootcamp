10270a352fcc850a87c84e1cdc8c911f768a3b3a
PickerWork <pickerwork@gmail.com>
2023-12-10 17:38:10 +0300

    Service

--- springProject/src/main/java/com/tobeto/springProject/controllers/BrandsController.java
+++ springProject/src/main/java/com/tobeto/springProject/controllers/BrandsController.java
@@ -1,59 +1,28 @@
 package com.tobeto.springProject.controllers;
 
+import com.tobeto.springProject.services.abstracts.BrandService;
 import com.tobeto.springProject.services.dtos.request.brand.AddBrandRequest;
-import com.tobeto.springProject.services.dtos.responses.brand.GetBrandResponse;
 import com.tobeto.springProject.entities.Brand;
 import com.tobeto.springProject.repositories.BrandRepository;
+import com.tobeto.springProject.services.dtos.responses.brand.GetBrandResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/brands")
+@AllArgsConstructor
 public class BrandsController {
+  private final BrandService brandService;
 
-    //Dependecy => Bağımlılık
-    //Injection => Enjekte
-
-    private final BrandRepository brandRepository;
-    public BrandsController(BrandRepository brandRepository){
-        this.brandRepository = brandRepository;
-    }
-    @GetMapping
-    public List<Brand> getAll(){
-        return brandRepository.findAll();
-    }
-
-    @GetMapping("{id}") //kullanıcıya cevap giden kısım
+  @GetMapping("{id}")
     public GetBrandResponse getById(@PathVariable int id){
-        //Optional<T> ilgili filtreden veri dönmeyebilir bunu opsiyonel kılar.
-        Brand brand = brandRepository.findById(id).orElseThrow();
-        GetBrandResponse getBrandResponse = new GetBrandResponse();
-        getBrandResponse.setName(brand.getName());
-        return getBrandResponse;
-    }
-    @PostMapping
-    public void add(@RequestBody AddBrandRequest request){
-        Brand brand = new Brand();
-        brand.setName(request.getName());
+      return this.brandService.getById(id);
+  }
 
-        brandRepository.save(brand); //mapping işlemi manual mapping -
-    }
-
-    @PutMapping
-    public void update (@RequestBody AddBrandRequest addBrandRequest){}
-
-    @DeleteMapping("{id}")
-    public void delete(@PathVariable int id){
-        //kod buraya geliyorsa exception fırlatmamıştır..
-        //Brand brandToDelete = brandRepository.findById(id).orElseThrow();
-        //buraya özel kontroller
-        //brandRepository.delete(brandToDelete);
-        //aşağıdaki kod yukarıdaki yapıyı kullanır
-        brandRepository.deleteById(id);
-    }
-    //DTO = Data Transfer Object
-
-    //her istek için bir Request bir Response modeli bulunmalıdır.
-    //AddBrandResponse add(AddBrandRequest request) {}
+  @PostMapping
+    public void add (@RequestBody AddBrandRequest request){
+      this.brandService.add(request);
+  }
 }
--- springProject/src/main/java/com/tobeto/springProject/services/concretes/BrandManager.java
+++ springProject/src/main/java/com/tobeto/springProject/services/concretes/BrandManager.java
@@ -6,13 +6,17 @@ import com.tobeto.springProject.services.abstracts.BrandService;
 import com.tobeto.springProject.services.dtos.request.brand.AddBrandRequest;
 import com.tobeto.springProject.services.dtos.responses.brand.GetBrandResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
+@Service
 public class BrandManager implements BrandService {
 
     private final BrandRepository brandRepository;
     @Override
     public void add(AddBrandRequest request) {
+        if(request.getName().length()<2)
+            throw new RuntimeException("Marka adı 2 haneden kısa olamaz");
 
         Brand brand = new Brand();
         brand.setName(request.getName()); //kullanıcıdan brand bilgisini al > sunucuya istek gönder > requestle veritabanına yaz
